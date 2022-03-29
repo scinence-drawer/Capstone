1e72f07d6929cc5d73da739dd7462e62a6806f0f
韩雨庭 <13141003288@189.cn>
2022-03-29 17:28:27 +0800

    completed: 1. upload new chat page template in project 2. retain a block for vr frame insert in.
    uncompleted:
    1. integrate the vr frame into chat page

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route('/')
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            engine="text-davinci-001",
+            prompt=animal,
+            temperature=0.1,
+            max_tokens=100,
+        )
+        # return redirect(url_for("index", result=response.choices[0].text))
+        return response.choices[0].text
+
+    result = request.args.get("result")
+
+    return render_template("index_chat.html", result=result)
+
+@app.route('/vr')
 def hello_world():  # put application's code here
     return render_template("index.html")
 
 
 if __name__ == '__main__':
     app.run()
+# @app.route("/response",methods=("GET", "POST"))
+# def response(prompt):
+#
---
+++ static/chatPage/script.js
@@ -0,0 +1,77 @@
+const data = [
+    {text: 'Hello! 😄'},
+    {text: 'How\'s it going?'},
+    {text: 'Cool.'},
+    {text: 'Okay.'},
+    {text: '👍'},
+    {text: 'Kay, bye! 👋'}
+];
+
+Vue.component('bubble', {
+    props: ['bubbtext'],
+    template: `<div class="bubble">{{bubbtext}}</div>`
+})
+
+var res_message = "";
+var vm = new Vue({
+    el: '#app',
+    data: {
+        noResponses: false,
+        isTyping: false,
+        isThinking: false,
+        newMessage: '',
+        resMessage: "",
+        contents: [],
+
+    },
+    watch: {
+        isThinking: function () {
+            if (this.isThinking) {
+                setTimeout(moveChat, 100);
+            }
+        }
+    },
+
+    methods: {
+        addToChat: function () {
+
+            this.contents.push({text: this.newMessage, isUser: true});
+            this.isThinking = true;
+            console.log(this.newMessage);
+            var a = function (vm) {
+
+
+                $.post("/",
+                    {
+                        animal: vm.newMessage,
+                    },
+                    function (data, status) {
+                        if (data !== '') {
+                            vm.resMessage = data;
+                            vm.addNewResponse();
+
+                            vm.isThinking = false;
+                            // setTimeout(vm.addNewResponse(), 100);
+                        }
+                    });
+                vm.newMessage="";
+            }
+            a(this);
+
+
+        },
+        addNewResponse: function () {
+            // this.contents.push(this.resMessage);
+            this.contents.push({text: this.resMessage});
+
+        }
+    }
+})
+
+vm.contents.push(data[0]);
+vm.contents.push(data[1]);
+
+function moveChat() {
+    const wrap = document.getElementById('wrapper');
+    wrap.scrollTop = wrap.scrollHeight;
+}
