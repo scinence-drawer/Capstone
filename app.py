5e1199c19a851c3c6d2774dc6dc2bd82132c509f
quesheng12 <yiran.zhao@ucdconnect.ie>
2022-03-30 14:45:39 +0800

    zyr

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+import os
+
+import openai
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template("index.html")
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            engine="text-davinci-001",
+            prompt=animal,
+            temperature=0.1,
+            max_tokens=100,
+        )
+        # return redirect(url_for("index", result=response.choices[0].text))
+        return response.choices[0].text
+
+    result = request.args.get("result")
+
+    return render_template("index.html", result=result)
 
 
 if __name__ == '__main__':
     app.run()
+# @app.route("/response",methods=("GET", "POST"))
+# def response(prompt):
+#
